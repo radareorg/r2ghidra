bf8a88c93b179b7e167dc0c4c3aff660573c84e0
pancake <pancake@nopcode.org>
2020-12-27 02:33:17 +0100

    Another fix

--- ghidra/deps.mk
+++ ghidra/deps.mk
@@ -152,8 +152,8 @@ GHIDRA_OBJS2=$(addprefix $(GHIDRA_DECOMPILER)/,$(BISON_OBJS) slgh_compile.o)
 sleigh: sleighc
 	$(SLEIGHC) $(SPECFILE) $(SLAFILE)
 
-sleighc: $(GHIDRA_DECOMPILER)/slgh_compile.cc $(GHIDRA_DECOMPILER)/slghscan.o $(GHIDRA_DECOMPILER)/slghparse.o
-	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o sleighc $(GHIDRA_DECOMPILER)/slgh_compile.cc $(GHIDRA_OBJS) $(GHIDRA_OBJS2)
+sleighc: $(GHIDRA_DECOMPILER)/slgh_compile.o $(GHIDRA_DECOMPILER)/slghscan.o $(GHIDRA_DECOMPILER)/slghparse.o
+	$(CXX) $(CXXFLAGS) $(LDFLAGS) -o sleighc $(GHIDRA_OBJS) $(GHIDRA_OBJS2)
 
 GHIDRA_SLEIGH_SLASPECS=$(GHIDRA_HOME)/Ghidra/Processors/*.slaspec
 GHIDRA_SLEIGH_FILES=$(GHIDRA_HOME)/Ghidra/Processors/*.cspec
