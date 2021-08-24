dc0e3b1e74f0c4c14e8795793a2446c5a8f55f9b
pancake <pancake@nopcode.org>
2021-08-25 01:35:03 +0200

    Add sleigh files in the Windows zip

--- make.bat
+++ make.bat
@@ -1,4 +1,6 @@
 cmake --build .
 copy *.dll ..\destdir
+mkdir ..\destdir\r2ghidra_sleigh
+copy ..\ghidra-native\src\Processors\* ..\destdir\r2ghidra_sleigh
 cd ..\destdir
 zip -r ..\r2ghidra-w64.zip *
