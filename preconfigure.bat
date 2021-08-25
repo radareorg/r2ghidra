a487fece72bbc7b2dbc08cfd65d8e26edd66427b
pancake <pancake@nopcode.org>
2021-08-25 13:26:10 +0200

    quiet unzip

--- preconfigure.bat
+++ preconfigure.bat
@@ -4,7 +4,7 @@ set ARCH=x64
 git submodule update --init
 
 python -m wget https://github.com/radareorg/ghidra-native/releases/download/0.1.0/ghidra-native-0.1.0.zip
-unzip ghidra-native-0.1.0.zip
+unzip -q ghidra-native-0.1.0.zip
 ren ghidra-native-0.1.0 ghidra-native
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
