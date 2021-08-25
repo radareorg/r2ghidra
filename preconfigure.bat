46ba68cb65f4b239f7b4734fc82ecdf1ed22af76
pancake <pancake@nopcode.org>
2021-08-25 12:37:47 +0200

    jasdklf

--- preconfigure.bat
+++ preconfigure.bat
@@ -3,6 +3,8 @@ set PATH=%CD%\radare2\bin;%PATH%
 set ARCH=x64
 git submodule update --init
 
-make ghidra-native
+python -m wget https://github.com/radareorg/ghidra-native/releases/download/0.1.0/ghidra-native-0.1.0.zip
+unzip ghidra-native-0.1.0.zip
+ren ghidra-native-0.1.0 ghidra-native
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
