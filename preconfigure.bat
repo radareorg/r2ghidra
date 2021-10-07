fd030e2b2b532fda02709164b04f1e89a94e1edd
pancake <pancake@nopcode.org>
2021-10-07 17:56:58 +0200

    Update paths to support more visual studios

--- preconfigure.bat
+++ preconfigure.bat
@@ -7,4 +7,32 @@ python -m wget https://github.com/radareorg/ghidra-native/releases/download/0.1.
 unzip -q ghidra-native-0.1.0.zip
 ren ghidra-native-0.1.0 ghidra-native
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+echo === Finding Visual Studio...
+cl --help > NUL 2> NUL
+if %ERRORLEVEL% == 0 (
+  echo FOUND
+) else (
+  if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community" (
+    echo "Found community edition"
+    call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
+  ) else (
+    if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" (
+      echo "Found Enterprise edition"
+      call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
+    ) else (
+      if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" (
+        echo "Found Professional edition"
+        call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
+      ) else (
+        if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" (
+          echo "Found BuildTools"
+          call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" %VSARCH%
+        ) else (
+          echo "Not Found"
+          exit /b 1
+        )
+      )
+    )
+  )
+)
