3e5441580a31d73faaa04f3e364a9263be549624
pancake <pancake@nopcode.org>
2022-03-24 11:48:01 +0100

    Upgrade to the latest ghidra-native 0.1.8

--- preconfigure.bat
+++ preconfigure.bat
@@ -3,9 +3,9 @@ set PATH=%CD%\radare2\bin;%PATH%
 set ARCH=x64
 git submodule update --init
 
-python -m wget https://github.com/radareorg/ghidra-native/releases/download/0.1.0/ghidra-native-0.1.0.zip
-unzip -q ghidra-native-0.1.0.zip
-ren ghidra-native-0.1.0 ghidra-native
+python -m wget https://github.com/radareorg/ghidra-native/releases/download/0.1.8/ghidra-native-0.1.8.zip
+unzip -q ghidra-native-0.1.8.zip
+ren ghidra-native-0.1.8 ghidra-native
 
 REM call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 echo === Finding Visual Studio...
