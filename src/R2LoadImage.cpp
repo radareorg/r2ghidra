8003c16039fe01adef1422595fc4f7a749a38999
pancake <pancake@nowsecure.com>
2022-07-30 20:17:17 -0400

    r2ghidra.roprop is now a number from 0 to 4

--- src/R2LoadImage.cpp
+++ src/R2LoadImage.cpp
@@ -1,6 +1,7 @@
-/* r2ghidra - LGPL - Copyright 2019-2021 - pancake, thestr4ng3r */
+/* r2ghidra - LGPL - Copyright 2019-2022 - pancake, thestr4ng3r */
 
 #include "R2LoadImage.h"
+#include "R2Utils.h"
 #include "R2Architecture.h"
 
 R2LoadImage::R2LoadImage(RCoreMutex *coreMutex, AddrSpaceManager *addr_space_manager) : LoadImage("radare2_program"),
@@ -22,12 +23,94 @@ string R2LoadImage::getArchType() const {
 void R2LoadImage::adjustVma(long adjust) {
 	throw LowlevelError("Cannot adjust radare2 virtual memory");
 }
+static bool isptr(ut64 *p) {
+	//if (!*p) return false;
+	if (*p < 0x1000) { return false; }
+	return (*p != UT64_MAX);
+}
 
 void R2LoadImage::getReadonly(RangeList &list) const {
-	// consider ANY address as resolable as a flag by r2
-	// this is used by the ropropagate code which follows
-	// pointers and replaces them with strings or flags
-	// in the decompilation. NULL is not considered.
-	auto space = addr_space_manager->getDefaultCodeSpace();
-	list.insertRange(space, 0x1000, ST64_MAX);
+	RCoreLock core(coreMutex);
+	int roprop = r_config_get_i (core->config, "r2ghidra.roprop");
+	if (roprop > 0) {
+		auto space = addr_space_manager->getDefaultCodeSpace ();
+		switch (roprop) {
+		case 1:
+			{
+				// find ranges with pointers
+				RIOMapRef *mapref;
+				RListIter *iter;
+				RIO *io = core->io;
+				RIOBank *bank = r_io_bank_get (io, io->bank);
+				r_list_foreach_cpp<RIOMapRef>(bank->maprefs, [&](RIOMapRef *mapref) {
+					RIOMap *map = r_io_map_get (io, mapref->id);
+					ut64 begin = r_io_map_begin (map);
+					ut64 end = r_io_map_end (map);
+					if (map->perm & R_PERM_W) {
+						return;
+					}
+					ut64 fin = end - begin;
+					if (fin > 0xffffff) {
+						list.insertRange(space, begin, end);
+						return;
+					}
+					ut8 *buf = (ut8 *)malloc (fin);
+					if (!buf) {
+						list.insertRange(space, begin, end);
+						return;
+					}
+					r_io_read_at (io, begin, buf, fin);
+					ut64 base = begin;
+					ut64 basefin = begin;
+					ut8 *data = buf;
+					bool hasdata = false;
+					int inc = (core->rasm->config->bits == 64)? 8: 4;
+					for (int i = 0; i < fin; i += inc) {
+						basefin = begin + i;
+						if (isptr ((ut64 *)(data + i))) {
+							// eprintf ("valid %llx\n", data[i]);
+							hasdata = true;
+						} else {
+							if (hasdata) {
+								// eprintf ("0 append %llx %llx because of 0x%llx\n", base, basefin, *((ut64*)(data +i)));
+								if (base > 0) {
+									list.insertRange(space, base, basefin); // begin, end);
+								}
+								hasdata = false;
+								base = basefin;
+							}
+						}
+					}
+					free (buf);
+					if (hasdata) {
+						// eprintf ("1 append %llx %llx\n", base, basefin);
+						list.insertRange(space, base, basefin);
+					}
+				});
+			}
+			break;
+		case 2:
+			{
+				RIOMapRef *mapref;
+				RListIter *iter;
+				RIO *io = core->io;
+				RIOBank *bank = r_io_bank_get (io, io->bank);
+				r_list_foreach_cpp<RIOMapRef>(bank->maprefs, [&](RIOMapRef *mapref) {
+					RIOMap *map = r_io_map_get (io, mapref->id);
+					ut64 begin = r_io_map_begin (map);
+					ut64 end = r_io_map_end (map);
+					if (map->perm & R_PERM_W) {
+						return;
+					}
+					list.insertRange(space, begin, end);
+				});
+			}
+		case 3:
+			list.insertRange(space, 0x1000, ST64_MAX - 1);
+			break;
+		default:
+			list.insertRange(space, 0, UT64_MAX - 1);
+			break;
+		}
+	}
 }
--- src/core_ghidra.cpp
+++ src/core_ghidra.cpp
@@ -67,7 +67,7 @@ static const ConfigVar cfg_var_maximplref   ("maximplref",  "2",        "Maximum
 static const ConfigVar cfg_var_rawptr       ("rawptr",      "true",     "Show unknown globals as raw addresses instead of variables");
 static const ConfigVar cfg_var_verbose      ("verbose",     "false",    "Show verbose warning messages while decompiling");
 static const ConfigVar cfg_var_casts        ("casts",       "false",    "Show type casts where needed");
-static const ConfigVar cfg_var_ropropagate  ("roprop",      "true",     "Propagate read-only constants");
+static const ConfigVar cfg_var_ropropagate  ("roprop",      "1",        "Propagate read-only constants (0,1,2,3,4)");
 static const ConfigVar cfg_var_timeout      ("timeout",     "0",        "Run decompilation in a separate process and kill it after a specific time");
 
 
