06d9a90dc96c40e39ac422f97effe42cec313498
pancake <pancake@nopcode.org>
2023-01-23 18:45:48 +0100

    Create offline source tarball

---
+++ dist/tarball/tarball.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+V=`../../configure -qV`
+if [ -z "${V}" ] ; then
+	echo "Cant run ./configure -qV"
+	exit 1
+fi
+rm -rf r2ghidra-${V}
+git clone ../.. r2ghidra-${V} || exit 1
+cd r2ghidra-${V} || exit 1
+./preconfigure || exit 1
+rm -rf .git ghidra-native/.git third-party/pugixml/.git
+cd ..
+rm -f r2ghidra-${V}.tar.xz
+tar cJvf r2ghidra-${V}.tar.xz r2ghidra-${V}
+rm -f r2ghidra-${V}.zip
+zip -9 -r r2ghidra-${V}.zip r2ghidra-${V}
+rm -rf r2ghidra-${V}
