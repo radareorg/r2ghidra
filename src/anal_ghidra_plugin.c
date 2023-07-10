eb3876bb209fa02afbff0fd63fb82b4ad2d7705b
pancake <pancake@nopcode.org>
2023-07-10 18:31:51 +0200

    Blind fix

--- src/anal_ghidra_plugin.c
+++ src/anal_ghidra_plugin.c
@@ -8,13 +8,13 @@
 #define RAnalEsil REsil
 #endif
 
-extern bool sanal_init(void *p);
-extern bool sanal_fini(void *p);
-extern RList *anal_preludes(RAnal *anal);
 
 #if R2_VERSION_NUMBER >= 50809
 extern int archinfo(RArchSession *as, ut32 query);
+extern bool sanal_init(void *p);
+extern bool sanal_fini(void *p);
 extern RList *r2ghidra_preludes(RArchSession *as);
+extern int archinfo(RArchSession *as, ut32 query);
 extern char *r2ghidra_regs(RArchSession *as);
 extern bool r2ghidra_esilcb(RArchSession *as, RArchEsilAction action);
 // extern bool sleigh_decode(RArchSession *as, RAnal *a, RAnalOp *anal_op, ut64 addr, const ut8 *data, int len, RAnalOpMask mask);
@@ -44,8 +44,11 @@ RArchPlugin r_arch_plugin_ghidra = {
 };
 #else
 
-extern int archinfo(RAnal *anal, ut32 query);
+extern RList *anal_preludes(RAnal *anal);
+extern int sanal_init(void *p);
+extern int sanal_fini(void *p);
 extern RList *r2ghidra_preludes(RAnal *anal);
+extern int archinfo(RAnal *a, int query);
 extern char *get_reg_profile(RAnal *anal);
 extern int sleigh_op(RAnal *a, RAnalOp *anal_op, ut64 addr, const ut8 *data, int len, RAnalOpMask mask);
 extern int esil_sleigh_fini(RAnalEsil *esil);
