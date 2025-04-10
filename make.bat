d5d6b63a5a817ed85200301717d184797157cf9d
satk0 <satko_a@op.pl>
2025-04-10 14:09:18 +0200

    Make w64 build fail

--- make.bat
+++ make.bat
@@ -4,18 +4,23 @@ if EXIST w (
 	configure.bat
 )
 
-ninja -C w
+call ninja -C w -j 2 && (
+  echo Copying Plugin
+  mkdir destdir
+  copy w\*.dll destdir
 
-echo Copying Plugin
-mkdir destdir
-copy w\*.dll destdir
+  REM echo Copying Sleigh Files
+  REM mkdir destdir\r2ghidra_sleigh
+  REM copy ghidra-native\src\Processors\*.* destdir\r2ghidra_sleigh
 
-REM echo Copying Sleigh Files
-REM mkdir destdir\r2ghidra_sleigh
-REM copy ghidra-native\src\Processors\*.* destdir\r2ghidra_sleigh
+  echo Ziping it Up
+  cd destdir
+  python -m zipfile -c r2ghidra-w64.zip core_r2ghidra.dll
+  dir
+  cd ..
+  exit /b 0
+) || (
+  echo Ninja compilation has failed
+  exit /b 1
+)
 
-echo Ziping it Up
-cd destdir
-python -m zipfile -c r2ghidra-w64.zip core_r2ghidra.dll
-dir
-cd ..
