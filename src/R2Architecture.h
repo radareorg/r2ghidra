3e3c6c2859b4e46ec1ae8c1fe7ac24ae3a2062d3
satk0 <satko_a@op.pl>
2025-05-27 12:03:34 +0200

    MSVC what u want from me?

--- src/R2Architecture.h
+++ src/R2Architecture.h
@@ -8,22 +8,22 @@
 
 #include "RCoreMutex.h"
 
-using namespace ghidra;
+// using namespace ghidra;
 
 class R2TypeFactory;
 typedef struct r_core_t RCore;
 
-class R2Architecture : public SleighArchitecture {
+class R2Architecture : public ghidra::SleighArchitecture {
 private:
 	RCoreMutex coreMutex;
 
 	R2TypeFactory *r2TypeFactory_ = nullptr;
-	std::map<std::string, VarnodeData> registers;
+	std::map<std::string, ghidra::VarnodeData> registers;
 	std::vector<std::string> warnings;
 
 	bool rawptr = false;
 
-	void loadRegisters(const Translate *translate);
+	void loadRegisters(const ghidra::Translate *translate);
 
 public:
 	explicit R2Architecture(RCore *core, const std::string &sleigh_id);
@@ -32,24 +32,24 @@ public:
 
 	R2TypeFactory *getTypeFactory() const { return r2TypeFactory_; }
 
-	ProtoModel *protoModelFromR2CC(const char *cc);
-	Address registerAddressFromR2Reg(const char *regname);
+	ghidra::ProtoModel *protoModelFromR2CC(const char *cc);
+	ghidra::Address registerAddressFromR2Reg(const char *regname);
 
 	void addWarning(const std::string &warning)	{ warnings.push_back(warning); }
 	const std::vector<std::string> getWarnings() const { return warnings; }
-	ContextDatabase *getContextDatabase();
+	ghidra::ContextDatabase *getContextDatabase();
 
 	void setRawPtr(bool rawptr) { this->rawptr = rawptr; }
 
 protected:
-	Translate *buildTranslator(DocumentStorage &store) override;
-	void buildLoader(DocumentStorage &store) override;
-	Scope *buildDatabase(DocumentStorage &store) override;
-	void buildTypegrp(DocumentStorage &store) override;
-	void buildCoreTypes(DocumentStorage &store) override;
-	void buildCommentDB(DocumentStorage &store) override;
+	ghidra::Translate *buildTranslator(ghidra::DocumentStorage &store) override;
+	void buildLoader(ghidra::DocumentStorage &store) override;
+	ghidra::Scope *buildDatabase(ghidra::DocumentStorage &store) override;
+	void buildTypegrp(ghidra::DocumentStorage &store) override;
+	void buildCoreTypes(ghidra::DocumentStorage &store) override;
+	void buildCommentDB(ghidra::DocumentStorage &store) override;
 	void postSpecFile() override;
-	void buildAction(DocumentStorage &store) override;
+	void buildAction(ghidra::DocumentStorage &store) override;
 };
 
 #endif //R2GHIDRA_R2ARCHITECTURE_H
