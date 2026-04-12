d0467530ccafeb20519f5b05fd9ade2bdf29dd03
pancake <pancake@nopcode.org>
2026-04-12 20:46:01 +0200

    Release 6.1.4

--- dist/windows/r2pm/r2pm-i.ps1
+++ dist/windows/r2pm/r2pm-i.ps1
@@ -1,6 +1,6 @@
 # powershell -executionpolicy unrestricted -command r2pm.ps1
 $R2_USER_PLUGINS=(radare2 -H R2_USER_PLUGINS)
-$V="6.1.2"
+$V="6.1.4"
 Remove-Item r2ghidra-${V}-w64.zip
 Remove-Item r2ghidra_sleigh-${V}.zip
 echo "Downloading blobs"
