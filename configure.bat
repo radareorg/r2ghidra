84b95f2be6b2c2873b264bc40e9bbe89e925218c
pancake <pancake@nopcode.org>
2026-06-20 06:07:40 +0200

    Do release builds with the configure.bat

--- configure.bat
+++ configure.bat
@@ -1,3 +1,7 @@
 @echo OFF
-echo "Configuring the build directory with cmake"
-meson w
+echo "Configuring the build directory with meson"
+if EXIST w\meson-private\coredata.dat (
+  meson setup w --reconfigure --buildtype=release
+) else (
+  meson setup w --buildtype=release
+)
